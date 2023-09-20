7d78ede66a947145280ad9006fe1aab55e6e7129
N-225\N-225 <jerpi053@gmail.com>
2023-09-20 11:26:04 +0800

    로그인버튼 추가

--- src/component/Appbar.js
+++ src/component/Appbar.js
@@ -146,6 +146,10 @@ const darkTheme = createTheme({
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
+                <Button variant="login">Login</Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
