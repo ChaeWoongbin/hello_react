5a5347e7d2fe31f6d72ed34ac1b0691b700c2821
N-225\N-225 <jerpi053@gmail.com>
2023-09-20 10:57:39 +0800

    Appbar 추가, 버튼 라우팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {Link} from "react-router-dom";
+import Appbar from './component/Appbar';
 import {Route, Routes} from 'react-router-dom';
 import Home from "./component/Home";
 import Login from "./component/Login";
@@ -15,30 +14,23 @@ import About from './component/About';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <header className="App-header">
+        <Appbar/>        
+      </header>
+      <body>
         <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="Home" element={<Home />}></Route>
         <Route path="*" element={<Wrong />}></Route>
         <Route path="Login" element={<Login />}></Route>
         <Route path="Profile" element={<Profile />}></Route>
         <Route path="About" element={<About />}></Route>
+        <Route path="Menu1" element={<Menu1_1 />}></Route>
+        <Route path="Menu2" element={<Menu2_1 />}></Route>
         <Route path="Menu1_1" element={<Menu1_1 />}></Route>
         <Route path="Menu1_2" element={<Menu1_2 />}></Route>
         <Route path="Menu2_1" element={<Menu2_1 />}></Route>
-      </Routes>
-      </header>
+      </Routes></body>
     </div>
   );
 }
