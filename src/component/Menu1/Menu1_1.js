865fc5923c48aa2b7f4defc8d16132865b389461
N-225 <jerpi053@gmail.com>
2023-10-31 11:54:36 +0900

    Menu 1_1 텍스트 입력 테스트

--- src/component/Menu1/Menu1_1.js
+++ src/component/Menu1/Menu1_1.js
@@ -1,12 +1,37 @@
+import { TextField } from '@mui/material';
 import React from 'react';
+import {useState} from 'react';
+
 
 
 const Menu1_1 = () => {
 
+  const [text, setText] = useState('');
+
+  const f_setText = event => {
+    setText(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
 
   return (
     <div className="App">
         Menu1_1 Page
+
+        
+
+        <div>텍스트 입력 테스트
+          <br/>
+      <TextField
+        type="text"
+        id="message"
+        name="message"
+        onChange={f_setText}
+        value={text}
+      />
+
+      <h2>입력 텍스트: {text}</h2>
+    </div>
     </div>
   );
 }
